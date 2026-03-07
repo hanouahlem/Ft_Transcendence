4b14b97bf6079f168e26c616e21b08c91193bcae
Nabil A <abboud.nabil@outlook.com>
2026-03-07 16:55:36 +0100

    ADD: login test + databases

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("Backend running");
+});
+
+app.listen(3001, ()=>{
+    console.log("Server running on port 3001");
+});
