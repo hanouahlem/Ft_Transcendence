cf90580af9ee303f4144b0703f722c9f916a8500
Ahlem Bey <ahbey@f4r6s2.clusters.42paris.fr>
2026-03-09 13:16:14 +0100

    norme fichier controller.js

--- Backend/src/controllers/controller.js
+++ Backend/src/controllers/controller.js
@@ -1,10 +1,11 @@
 
 import dotenv from 'dotenv';
-dotenv.config();
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+
+dotenv.config();
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
