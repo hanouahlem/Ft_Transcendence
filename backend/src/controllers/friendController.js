9488107b888b8db515a4c1e7b4c98659736f1f59
Manar Bengharbi <manbengh@f4r12s7.clusters.42paris.fr>
2026-03-17 17:11:35 +0100

    UPDATE : notif files

--- backend/src/controllers/friendController.js
+++ backend/src/controllers/friendController.js
@@ -140,6 +140,9 @@ export async function acceptFriend(req, res) {
 export async function deleteFriend(req, res) {
     try {
         const userId = req.user.id;
+        if (!userId) {
+            return res.status(400).json({ message: "userId is required" });
+        }
 
         const friendId = parseInt(req.params.id); // ← req.params.id pas req.body
 
@@ -168,6 +171,9 @@ export async function deleteFriend(req, res) {
 export async function getFriendRequests(req, res) {
     try {
         const userId = req.user.id;
+        if (!userId) {
+            return res.status(400).json({ message: "userId is required" });
+        }
 
         const requests = await prisma.friends.findMany({
             where: {
--- backend/src/controllers/notifController.js
+++ backend/src/controllers/notifController.js
@@ -13,6 +13,9 @@ export async function createNotif(req, res){
         const userId = req.user.id;
         const {content} = req.body;
 
+        if (!userId) {
+            return res.status(400).json({ message: "userId is required" });
+        }
         if (!content) {
             return res.status(400).json({ message: "content is required" });
         }
@@ -35,26 +38,84 @@ export async function createNotif(req, res){
 
 export async function getNotif(req, res){
 
-    const userId = req.user.id;
-        const {content} = req.body;
+    try {
+        const userId = req.user.id;
+        if (!userId) {
+            return res.status(400).json({ message: "userId is required" });}
 
-        if (!content) {
-            return res.status(400).json({ message: "content is required" });
-        }
-
-        
+        const allNotifs = await prisma.notification.findMany({
+            where: { userId },
+            orderBy: {createdAt: 'desc'}
+        })
+        return res.status(200).json({allNotifs});
+    }
+    catch (error){
+        console.error("getNotif error: ", error);
+        return res.status(500).json({ message: "Failed to get a notification" });
+    }  
 }
 
 
 export async function markAsRead(req, res){
+    try {
+        const userId = req.user.id;
+        if (!userId) {
+            return res.status(400).json({ message: "userId is required" });
+        }
 
+        const notifId = parseInt(req.params.id);
+
+        const notification = await prisma.notification.findFirst({
+            where: { id: notifId , userId }
+        });
+
+        if (!notification) {
+            return res.status(404).json({ message: "Notification not found"});
+        }
+
+        const updatedNotif = await prisma.notification.update({
+            where: { id: notifId },
+            data: { read: true }
+        });
+
+        return res.status(200).json({ notification: updatedNotif});
+
+    }
+    catch (error){
+        console.error("markAsRead error: ", error);
+        return res.status(500).json({ message: "Failed to mark a notification as read" });
+    }
     
 }
 
 
 export async function deleteNotif(req, res){
 
-    
+    try {
+        const userId = req.user.id;
+        if (!userId) {
+            return res.status(400).json({ message: "userId is required" });
+        }
+
+        const notifId = parseInt(req.params.id);
+
+        const notification = await prisma.notification.findFirst({
+            where: { id: notifId , userId }
+        });
+        if (!notification) {
+            return res.status(404).json({ message: "Notification not found"});
+        }
+
+        await prisma.notification.delete({
+            where: { id: notification.id }
+        });
+
+        return res.status(200).json({ message: "Notification removed" });
+    }
+    catch (error){
+        console.error("deleteNotif error: ", error);
+        return res.status(500).json({ message: "Failed to delete a notification" });
+    }
 }
 
 export default { createNotif, getNotif, markAsRead, deleteNotif };
